262df23fdf891d7323c3e68c90e2c8774a7c9d39
Christian Colon <christian.colon27@gmail.com>
2021-08-11 23:57:24 -0400

    added functions to add and check quiz data

--- Assets/Script/script.js
+++ Assets/Script/script.js
@@ -1,11 +1,12 @@
 var quiz = document.getElementById("#quiz");
-var answerEl = document.getElementById("answer");
+var answerEl = document.querySelectorAll(".answer");
 var questionEl = document.getElementById("question");
 var optionA = document.getElementById("option-a");
 var optionB = document.getElementById("option-b");
 var optionC = document.getElementById("option-c");
 var optionD = document.getElementById("option-d");
-var submitBtn = document.getElementById("#sub,it");
+var submitBtn = document.getElementById("#submit");
+// object to store questions, options, and answers
 var quizDataObj = [
     {
         question: "What does CSS stand for?",
@@ -14,7 +15,7 @@ var quizDataObj = [
         c: "Coding Style Sheets",
         d: "Cascading Stat Sheets",
         correct: "b",
-    }
+    },
     {
         question: "What does HTML stand for?",
         a: "Hypertext Multi Language",
@@ -22,7 +23,7 @@ var quizDataObj = [
         c: "Hypertext Markup Language",
         d: "Hypertext Markup Linguistics",
         correct: "c",
-    }
+    },
     {
         question: "How do you call an id in CSS?",
         a: "#",
@@ -30,7 +31,7 @@ var quizDataObj = [
         c: "%",
         d: "*",
         correct: "a",
-    }
+    },
     {
         question: "How do you call a class in CSS?",
         a: "#",
@@ -38,7 +39,7 @@ var quizDataObj = [
         c: ".",
         d: "!",
         correct: "c",
-    }
+    },
     {
         question: "What year was javascript created?",
         a: "1998",
@@ -48,3 +49,29 @@ var quizDataObj = [
         correct: "d",
     }
 ]
+
+var currentQuiz = 0;
+var score = 0;
+
+loadQuestion()
+// function to select quiz questions and answers 
+function loadQuestion() {
+    deselectAnswer();
+    var currentQuizData = quizDataObj[currentQuiz]
+
+    questionEl.innerText = currentQuizData.question
+    optionA.innerText = currentQuizData.a
+    optionB.innerText = currentQuizData.b
+    optionC.innerText = currentQuizData.c
+    optionD.innerText = currentQuizData.d
+}
+
+function deselectAnswer() {
+    if (answerEl.checked !== correct) {
+        return false;
+    }
+}
+
+function getSelected() {
+    
+}
