6e9d3e04692abe456740711fc7658c7233ef0d86
Christian Colon <christian.colon27@gmail.com>
2021-08-17 22:44:25 -0400

    refactoring code

--- Assets/Script/script.js
+++ Assets/Script/script.js
@@ -1,70 +1,83 @@
-var quiz = document.getElementById("#quiz");
+// variables for DOM manipulation
+var quiz = document.getElementById("quiz");
 var answerEl = document.querySelectorAll(".answer");
 var questionEl = document.getElementById("question");
-var optionA = document.getElementById("option-a");
-var optionB = document.getElementById("option-b");
-var optionC = document.getElementById("option-c");
-var optionD = document.getElementById("option-d");
-var submitBtn = document.getElementById("#submit");
+// var optionA = document.getElementById("option-a");
+// var optionB = document.getElementById("option-b");
+// var optionC = document.getElementById("option-c");
+// var optionD = document.getElementById("option-d");
+var submitBtn = document.getElementById("submit");
+var starBtnEl = document.getElementById("startBtn")
+var counterEl = document.getElementById("counter");
+// variables to control quiz state
+var  questionIndex = 0
+var time = 75
+var counterId;
 // object to store questions, options, and answers
 var quizDataObj = [
     {
-        question: "What does CSS stand for?",
-        a: "Coding Style Statistics",
-        b: "Cascading Style Sheets",
-        c: "Coding Style Sheets",
-        d: "Cascading Stat Sheets",
-        correct: "b",
+        quote: "What does CSS stand for?",
+        choices: ["Coding Style Statistics","Cascading Style Sheets","Coding Style Sheets","Cascading Stat Sheets"],
+        correct: "Cascading Style Sheets",
     },
     {
-        question: "What does HTML stand for?",
-        a: "Hypertext Multi Language",
-        b: "History of Translating Markup Languages",
-        c: "Hypertext Markup Language",
-        d: "Hypertext Markup Linguistics",
-        correct: "c",
+        quote: "What does HTML stand for?",
+        choices: ["Hypertext Multi Language", "History of Translating Markup Languages","Hypertext Markup Language","Hypertext Markup Linguistics",],
+        correct: "Hypertext Markup Language",
     },
     {
-        question: "How do you call an id in CSS?",
-        a: "#",
-        b: ".",
-        c: "%",
-        d: "*",
-        correct: "a",
+        quote: "How do you call an id in CSS?",
+        choices: ["#",".","%","*"],
+        correct: "#",
     },
     {
-        question: "How do you call a class in CSS?",
-        a: "#",
-        b: "%",
-        c: ".",
-        d: "!",
-        correct: "c",
+        quote: "How do you call a class in CSS?",
+        choices: ["#","%",".","!"],
+        correct: ".",
     },
     {
-        question: "What year was javascript created?",
-        a: "1998",
-        b: "2001",
-        c: "1979",
-        d: "1995",
-        correct: "d",
+        quote: "What year was javascript created?",
+        choices: ["1998","2001","1979","1995"],
+        correct: "1995",
     }
 ]
 
-var currentQuiz = 0;
-var score = 0;
+// To DO:
+// create timer function
+function countdown() {
+    // decrement time by 1
+    time--;
+    counterEl.textContent = time;
+    if (time <= 0) {
+        endQuiz();
+    }
+}
+startQuiz;
+console.log(time);
+// create start quiz function
+function startQuiz() {
+    // start timer
+    counterId = setInterval(countdown, 1000)
+    counterEl.textContent = time;
+}
 
-loadQuestion()
+// create a function for event listener clicking a question
+// create end quiz function
+// create function for high scores and store to local storage
+
 // function to select quiz questions and answers 
 function loadQuestion() {
     deselectAnswer();
     var currentQuizData = quizDataObj[currentQuiz]
-
+    
     questionEl.innerText = currentQuizData.question
     optionA.innerText = currentQuizData.a
     optionB.innerText = currentQuizData.b
     optionC.innerText = currentQuizData.c
     optionD.innerText = currentQuizData.d
 }
+
+loadQuestion()
 
 function deselectAnswer() {
     if (answerEl.checked !== correct) {
